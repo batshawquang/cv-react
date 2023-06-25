17fc1a0a7512ec8cc1f3da5bdaaee5c29f4c6271
batshawquang <quang.nhu.dang@gmail.com>
2023-06-25 21:30:11 +0200

    adjust urls

--- src/Components/ProjectSection.js
+++ src/Components/ProjectSection.js
@@ -6,43 +6,43 @@ const ProjectSection = () => {
             <div className="grid grid-cols-3 gap-4">
                 <ProjectCard
                     colspan="col-span-2"
-                    linkTo="/project01"
+                    linkTo="/cv-react/project01"
                     imgSrc="/ProjectImgs/pro01.jpg"
                     name="Embedded PowerBi App"
                 />
                 <ProjectCard
                     colspan=""
-                    linkTo="/project02"
+                    linkTo="/cv-react/project02"
                     imgSrc="/ProjectImgs/pro02.jpg"
                     name="Microsoft WPF"
                 />
                 <ProjectCard
                     colspan=""
-                    linkTo="/project03"
+                    linkTo="/cv-react/project03"
                     imgSrc="/ProjectImgs/pro03.jpg"
                     name="Real-time Stereo Matching"
                 />
                 <ProjectCard
                     colspan="col-span-2"
-                    linkTo="/project04"
+                    linkTo="/cv-react/project04"
                     imgSrc="/ProjectImgs/pro04.jpg"
                     name="Light direction estimation"
                 />
                 <ProjectCard
                     colspan=""
-                    linkTo="/project05"
+                    linkTo="/cv-react/project05"
                     imgSrc="/ProjectImgs/pro05.jpg"
                     name="Landing Page with Bootstrap"
                 />
                 <ProjectCard
                     colspan=""
-                    linkTo="/project06"
+                    linkTo="/cv-react/project06"
                     imgSrc="/ProjectImgs/pro06.jpg"
                     name="Windows Forms .NET"
                 />
                 <ProjectCard
                     colspan=""
-                    linkTo="/project07"
+                    linkTo="/cv-react/project07"
                     imgSrc="/ProjectImgs/pro07.jpg"
                     name="Pokemon Deck"
                 />
