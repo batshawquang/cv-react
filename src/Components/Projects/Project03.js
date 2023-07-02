69a60623c7476f84fa83d76ca220616e57749326
Nhu Quang Dang <Nhu.Quang.Dang@EXXETA.com>
2023-07-02 12:16:14 +0200

    update content for project 3 and 4

--- src/Components/Projects/Project03.js
+++ src/Components/Projects/Project03.js
@@ -4,44 +4,44 @@ import BackHomeButton from "../BackHomeButton";
 const Project03 = () => {
     return(
         <div className="mx-14 my-5 max-w-screen-xl bg-gray-200 p-5 rounded">
-            <div className="grid grid-cols-2 gap-5">
-                <div className="">
-                    <img src="https://i.imgur.com/d3tFKqj.png" alt=""></img>
-                </div>
-                <div className="flex flex-col text-start">
-                    <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-                    <p>öajhdfliajs</p>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="flex flex-col text-start">
-                    <p>öasljhdfliajsöasljhdfliajsöasljjhdfl</p>
-                    <p>öaliajsöasljhdfliajsöasljhdfliajsöasljhdfliajsöasljhdfliajs</p>
-                </div>
-                <div className="">
-                    <img src="https://i.imgur.com/wn3jroF.png" alt=""></img>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="">
-                    <img src="https://i.imgur.com/hk9nLpz.png" alt=""></img>
-                </div>
-                <div className="flex flex-col text-start">
-                    <p>öashdfliajsöasljhdfliajsöasljhdfliajs</p>
-                    <p>öaöasljhdfliajsöasljhdfliajs</p>
+            <div className="mx-5">
+                <h1 className="text-2xl mb-5">Real-time Stereo Matching</h1>
+                <img src="https://i.imgur.com/d3tFKqj.png" alt="" className="rounded w-full"></img>
+                <div className="text-start mx-5 my-10">
+                    <p className=""><span className="font-bold">Aim:</span> Implement and evaluate two existing algowithms for real-time stereo matching.</p>
+                    <p className="font-bold">Description:</p>
+                    <ul className="list-disc list-inside">
+                        <li>3D Reconstruction of real model from stereo image pairs in real-time</li>
+                        <li>Using cameras to build a system to test with real object in lab environment</li>
+                    </ul>
+                    <p className="font-bold">Contribute:</p>
+                    <ul className="list-disc list-inside">
+                        <li>Contribute ideas to build the system pipeline</li>
+                        <li>Participate in implementing one of the two algowithms to create point cloud information</li>
+                        <li>Visualize point cloud in 3D</li>
+                        <li>Showing the project to visitors in summaery2019</li>
+                    </ul>
                 </div>
             </div>
-
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="flex flex-col text-start">
-                    <p>öasljhdfliajsöasljhdfliajsöasljjhdfl</p>
-                    <p>öaliajsöasljhdfliajsöasljhdfliajsöasljhdfliajsöasljhdfliajs</p>
+                
+            <div className="m-5 p-5 bg-gray-100 rounded-md">
+                <div className="grid grid-cols-2 my-5">
+                    <div className="flex justify-center">
+                        <img src="https://i.imgur.com/wn3jroF.png" alt="" className="rounded w-56"></img>
+                    </div>
+                    <div className="flex justify-center">
+                        <img src="https://i.imgur.com/hk9nLpz.png" alt="" className="rounded w-56"></img>
+                    </div>
                 </div>
-                <div className="">
+                <figcaption className="text-center">Stereo images</figcaption>
+            </div>
+            
+
+            <div className="m-5 p-5 bg-gray-100 rounded-md">
+                <div className="flex justify-center">
                     <img src="https://i.imgur.com/giH6UYc.png" alt=""></img>
                 </div>
+                <figcaption>3D Point Cloud</figcaption>
             </div>
 
             <BackHomeButton/>
--- src/Components/Projects/Project04.js
+++ src/Components/Projects/Project04.js
@@ -4,54 +4,47 @@ import BackHomeButton from "../BackHomeButton";
 const Project04 = () => {
     return(
         <div className="mx-14 my-5 max-w-screen-xl bg-gray-200 p-5 rounded">
-            <div className="grid grid-cols-2 gap-5">
-                <div className="">
-                    <img src="https://i.imgur.com/ewZQQQd.png" alt=""></img>
+            
+            <div className="mx-5">
+                <div className="flex justify-center">
+                    <img src="https://i.imgur.com/ewZQQQd.png" alt="" className="rounded-lg w-3/4"></img>
                 </div>
-                <div className="flex flex-col text-start">
-                    <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
-                    <p>öajhdfliajs</p>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="flex flex-col text-start">
-                    <p>öasljhdfliajsöasljhdfliajsöasljjhdfl</p>
-                    <p>öaliajsöasljhdfliajsöasljhdfliajsöasljhdfliajsöasljhdfliajs</p>
-                </div>
-                <div className="">
-                    <img src="https://i.imgur.com/qLAsVZi.png" alt=""></img>
-                </div>
-            </div>
-
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="">
-                    <img src="https://i.imgur.com/ebxINnM.png" alt=""></img>
-                </div>
-                <div className="flex flex-col text-start">
-                    <p>öashdfliajsöasljhdfliajsöasljhdfliajs</p>
-                    <p>öaöasljhdfliajsöasljhdfliajs</p>
+                <div className="text-start mx-5 my-10">
+                    <p className=""><span className="font-bold">Aim:</span> Implement and evaluate two existing algowithms for real-time stereo matching.</p>
+                    <p className="font-bold">Description:</p>
+                    <ul className="list-disc list-inside">
+                        <li>3D Reconstruction of real model from stereo image pairs in real-time</li>
+                        <li>Using cameras to build a system to test with real object in lab environment</li>
+                    </ul>
+                    <p className="font-bold">Contribute:</p>
+                    <ul className="list-disc list-inside">
+                        <li>Participate in implementing one of the two algowithms to create point cloud information</li>
+                    </ul>
                 </div>
             </div>
 
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="flex flex-col text-start">
-                    <p>öasljhdfliajsöasljhdfliajsöasljjhdfl</p>
-                    <p>öaliajsöasljhdfliajsöasljhdfliajsöasljhdfliajsöasljhdfliajs</p>
+            <div className="m-5 bg-gray-100 rounded-md">
+                <div className="flex justify-around my-5">
+                    <div className="m-1">
+                        <img src="https://i.imgur.com/qLAsVZi.png" alt="" className="rounded w-max"></img>
+                    </div>
+                    <div className="m-1">
+                        <img src="https://i.imgur.com/ebxINnM.png" alt="" className="rounded w-max"></img>
+                    </div>
                 </div>
-                <div className="">
-                    <img src="https://i.imgur.com/61WeeOl.png" alt=""></img>
-                </div>
+                <figcaption className="text-center">Lab Setups</figcaption>
             </div>
 
-            <div className="grid grid-cols-2 gap-5 mt-5">
-                <div className="">
-                    <img src="https://i.imgur.com/D0NZ18P.png" alt=""></img>
+            <div className="m-5 bg-gray-100 rounded-md">
+                <div className="flex justify-around my-5">
+                    <div className="m-1">
+                        <img src="https://i.imgur.com/61WeeOl.png" alt="" className="rounded w-max"></img>
+                    </div>
+                    <div className="m-1">
+                        <img src="https://i.imgur.com/D0NZ18P.png" alt="" className="rounded w-max"></img>
+                    </div>
                 </div>
-                <div className="flex flex-col text-start">
-                    <p>öashdfliajsöasljhdfliajsöasljhdfliajs</p>
-                    <p>öaöasljhdfliajsöasljhdfliajs</p>
-                </div>
+                <figcaption className="text-center">Final Results</figcaption>
             </div>
 
             <BackHomeButton/>
